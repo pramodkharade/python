c298021257a45ecc025f980165de2704af0d3044
pramod.kharade <pramod.kharade@blazeclan.com>
2018-11-01 18:11:02 +0530

    cinema mini project

--- basic/cinema.py
+++ basic/cinema.py
@@ -6,5 +6,20 @@ films = {
     }
 
 while True:
-    choice = input("What flim would you like to watch? :").strip()
+    choice = input("What flim would you like to watch? :").strip().title()
+    if choice in films:
+        age = int(input("How old are you?").strip())
+        #check user age
+        if age >= films[choice][0]:
+            #check enough seats
+            num_seats= films[choice][1]
+            if num_seats >0:
+              print("Enjoy the film!")
+              films[choice][1] = films[choice][1] - 1
+            else:
+                print("Sorry. we are sold out!")
+        else:
+            print("You are too young to see that film!")
+    else:
+        print("We don't have that film...")
     
